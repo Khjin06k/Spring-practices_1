2bbe26c198a1e71446e682df3fcd83f1e6c3c933
김희진 <khj4209kk@gmail.com>
2022-11-15 01:34:48 +0900

    구현 필요

---
+++ src/main/java/com/example/Coffee/controller/CoffeeController.java
@@ -0,0 +1,12 @@
+package com.example.Coffee.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/v1/coffees", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CoffeeController {
+    //커피 주문
+    public ResponseEntity
+
+}
