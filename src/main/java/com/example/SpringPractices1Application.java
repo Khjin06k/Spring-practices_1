518b6d98ff791d0c723bfeaff085ced242fc1b5e
김희진 <khj4209kk@gmail.com>
2022-11-15 01:31:41 +0900

    Initial commit

---
+++ src/main/java/com/example/SpringPractices1Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring.practices_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractices1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/practices_1/SpringPractices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.practices_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
