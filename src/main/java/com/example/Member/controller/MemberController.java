cac83fea587ab33c1df32600cf3656ec7372a876
김희진 <khj4209kk@gmail.com>
2022-11-16 00:35:02 +0900

    MemberController 회원 정보 수정, 회원 정보 삭제 핸들러 메서드 추가 구현

--- src/main/java/com/example/Member/controller/MemberController.java
+++ src/main/java/com/example/Member/controller/MemberController.java
@@ -1,40 +1,75 @@
 package com.example.Member.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
-@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/v1/members")
 public class MemberController {
+    private final Map<Long, Map<String, Object>> members = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        Map<String, Object> member1 = new HashMap<>();
+        long memberId = 1L;
+        member1.put("memberId", memberId);
+        member1.put("email", "khj@gmail.com");
+        member1.put("name", "김희진");
+        member1.put("phone", "010-1111-1111");
+
+        members.put(memberId, member1);
+    }
+
     //회원 등록
     @PostMapping
-    public String postMember(@RequestParam("email") String email,
+    public ResponseEntity postMember(@RequestParam("email") String email,
                              @RequestParam("name") String name,
                              @RequestParam("phone") String phone) {
-        System.out.println("# email : " + email);
-        System.out.println("# name : " + name);
-        System.out.println("# phone : " + phone);
 
-        String response = "{\"" + "email\" : \"" + email + "\"," +
-                "\"name\" : " + name + "\", \"" +
-                "\"phone\" : \"" + phone + "\"}";
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
 
-        return response;
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-Id") long memberId,
+                                      @RequestParam("phone") String phone){
+        Map<String, Object> map = members.get(memberId);
+        map.put("phone", phone);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
     //회원 조회
     @GetMapping("/{member-id}")
-    public String getMember(@PathVariable("member-id") long memberId){
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId){
         System.out.println("# memberId : " + memberId);
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getMembers(){
+    public ResponseEntity getMembers(){
         System.out.println("# get Members");
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId){
+        members.remove(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
