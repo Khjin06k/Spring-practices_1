4033587b1f5d4becfad8d6620db8f2153b1d0402
김희진 <khj4209kk@gmail.com>
2022-11-21 23:33:03 +0900

    MemberResponse 구현

--- src/main/java/com/example/Member/dto/MemberResponseDto.java
+++ src/main/java/com/example/Member/dto/MemberResponseDto.java
@@ -1,4 +1,13 @@
 package com.example.Member.dto;
 
-public class MemberResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberResponseDto  {
+    private long memberId;
+    private String email;
+    private String name;
+    private String phone;
 }
