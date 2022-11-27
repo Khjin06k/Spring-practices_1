48d3aef2e88a0c275c1ccc7219e8d414295d2be0
김희진 <khj4209kk@gmail.com>
2022-11-27 21:07:44 +0900

    JPA란 실습 학습

--- src/main/java/com/example/Member/entity/Member.java
+++ src/main/java/com/example/Member/entity/Member.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
-@AllArgsConstructor //모든 멤버 변수를 파라미터로 갖는 Member 생성자 자동 생성
+//@AllArgsConstructor //모든 멤버 변수를 파라미터로 갖는 Member 생성자 자동 생성
 @NoArgsConstructor // 파라미터가 없는 기본 생성자 자동 생성
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue
     private long memberId;
     private String email;
     private String name;
