6a38ba468057c86fcf5aba9eeaea2419e929fe26
김희진 <khj4209kk@gmail.com>
2022-12-04 17:03:56 +0900

    Member Entity JPA 구현 실습 진행

--- src/main/java/com/example/Member/entity/Member.java
+++ src/main/java/com/example/Member/entity/Member.java
@@ -23,17 +23,9 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long memberId;
 
-    public Member(Long memberId){
-        this.memberId = memberId;
-    }
-
     @Column(nullable = false, updatable = false, unique = true)
     //nullable : null 값 허용 여부, updatable : 수정 가능 여부, unique : 동일한 값 등록 여부
     private String email;
-
-    public Member(String email){
-        this.email = email;
-    }
 
     @Column(length = 100, nullable = false)
     //length : 문자열 길이 제한
@@ -53,18 +45,43 @@ public class Member {
     @OneToMany(mappedBy = "member") //일대다 매핑 진행, mappedBy에 들어갈 단어는 외래키이며 다에 해당하는 객체인지 확인
     private List<Order> orders = new ArrayList<>();
 
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false
+    )
+    private Member.MemberStatus memberStatus = Member.MemberStatus.MEMBER_ACTIVE;
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("활동 종료"),
+        MEMBER_QUIT("회원 탈퇴");
+        @Getter
+        private String staus;
+
+        MemberStatus(String staus) {
+            this.staus = staus;
+        }
+    }
+
+    public Member(Long memberId){
+        this.memberId = memberId;
+    }
+
+    public Member(String email){
+        this.email = email;
+    }
+
     public void addOrder(Order order){
         orders.add(order);
     }
-
-    @Transient
-    // 테이블 칼럼과 매핑하지 않겠다는 의미로 JPA 가 인식
-    // DB에 저장되지 않으며, 조회시 매핑되지 않기 때문에 임시 데이터를 메모리에 사용하기 위해서 사용함
-    private String age;
 
     public Member(String email, String name, String phone) {
         this.email = email;
         this.name = name;
         this.phone = phone;
     }
+
+    /*@Transient
+    // 테이블 칼럼과 매핑하지 않겠다는 의미로 JPA 가 인식
+    // DB에 저장되지 않으며, 조회시 매핑되지 않기 때문에 임시 데이터를 메모리에 사용하기 위해서 사용함
+    private String age;*/
+
 }
