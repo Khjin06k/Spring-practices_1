ad62b295dbd08915c5e33ea5b2d04354b12e920e
김희진 <khj4209kk@gmail.com>
2022-12-04 14:25:37 +0900

    Coffee 클래스 엔티티 매핑

--- src/main/java/com/example/Coffee/entity/Coffee.java
+++ src/main/java/com/example/Coffee/entity/Coffee.java
@@ -5,13 +5,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
+@Entity
 public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long coffeeId;
+
+    @Column(nullable = false, length = 50)
     private String engName;
+
+    @Column(nullable = false, length = 50)
     private String korName;
+
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false, length = 3)
+    private String coffeeCode;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
