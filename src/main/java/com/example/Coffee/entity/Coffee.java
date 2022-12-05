28886d6100373de47446054b48f2d403bb58d612
김희진 <khj4209kk@gmail.com>
2022-12-05 23:29:55 +0900

    Coffee Entity JPA 구현 실습 진행

--- src/main/java/com/example/Coffee/entity/Coffee.java
+++ src/main/java/com/example/Coffee/entity/Coffee.java
@@ -18,21 +18,37 @@ public class Coffee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long coffeeId;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String engName;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String korName;
 
     @Column(nullable = false)
     private int price;
 
-    @Column(nullable = false, length = 3)
+    @Column(nullable = false, length = 3, unique = true)
     private String coffeeCode;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private CoffeeStatus coffeeStatus = CoffeeStatus.COFFEE_FOR_SALE;
 
     @Column(nullable = false)
     private LocalDateTime createAt = LocalDateTime.now();
 
     @Column(nullable = false, name = "LAST_MODIFIED_AT")
     private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    public enum CoffeeStatus {
+        COFFEE_FOR_SALE("판매중"),
+        COFFEE_SOLD_OUT("판매중지");
+
+        @Getter
+        private String status;
+
+        CoffeeStatus(String status) {
+            this.status = status;
+        }
+    }
 }
