f1970e05d9bb667dea6a1a6a9bf83fb2e9572c5c
김희진 <khj4209kk@gmail.com>
2022-12-09 21:42:25 +0900

    OrderMapper 구현

---
+++ src/main/java/com/example/Order/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.example.Order.mapper;
+
+import com.example.Order.dto.OrderPatchDto;
+import com.example.Order.dto.OrderPostDto;
+import com.example.Order.dto.OrderResponseDto;
+import com.example.Order.entity.Order;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+    Order orderPostDtoToOrder(OrderPostDto orderPostDto);
+    Order orderPatchDtoToOrder(OrderPatchDto orderPatchDto);
+    List<OrderResponseDto> orderToOrderResponseDtos(List<Order> orders);
+}
